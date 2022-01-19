98a6d29e487af65d17f8a56c6758acb7c684a2a9
RAJATsd <rajatsharmaatri@gmail.com>
2022-01-19 20:40:54 +0530

    added the functionality of redirecting to the link of the news

--- src/components/singleNewsCard/singleNewsCard.component.jsx
+++ src/components/singleNewsCard/singleNewsCard.component.jsx
@@ -6,30 +6,32 @@ export default class SingleNewsCard extends Component {
     const cardDetails = this.props.articleDetails;
 
     return (
-      <div className="single-card">
-        <div className="news-image-container">
-          <img
-            id="news-related-image"
-            src={cardDetails.urlToImage}
-            alt="related"
-          />
-        </div>
-        <div className="news-info">
-          <h5 className="news-title">{cardDetails.title}</h5>
-          <div className="news-author">
-            by{" "}
-            <a id="news-author-name" href="#">
-              {cardDetails.author}
-            </a>
+      <a className="news-redirecting-link" href={cardDetails.url}>
+        <div className="single-card">
+          <div className="news-image-container">
+            <img
+              id="news-related-image"
+              src={cardDetails.urlToImage}
+              alt="related"
+            />
           </div>
-          <div className="news-description">
-            {cardDetails.content.length < 120
-              ? cardDetails.content
-              : cardDetails.content.slice(0, 120) + "..."}
+          <div className="news-info">
+            <h5 className="news-title">{cardDetails.title}</h5>
+            <div className="news-author">
+              by{" "}
+              <a id="news-author-name" href="#">
+                {cardDetails.author}
+              </a>
+            </div>
+            <div className="news-description">
+              {cardDetails.content.length < 120
+                ? cardDetails.content
+                : cardDetails.content.slice(0, 120) + "..."}
+            </div>
+            <div className="pusblishing-time">{cardDetails.publishedAt}</div>
           </div>
-          <div className="pusblishing-time">{cardDetails.publishedAt}</div>
         </div>
-      </div>
+      </a>
     );
   }
 }
